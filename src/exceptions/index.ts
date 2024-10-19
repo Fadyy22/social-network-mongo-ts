437d8797a4d2a16e1be6db058c1b90b8f149fe89
Fadyy22 <fadyalaa441@gmail.com>
2024-10-19 19:15:10 +0300

    add ConflictException

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -2,10 +2,12 @@ import NotFoundException from './NotFoundException';
 import BadRequestException from './BadRequestException';
 import UnauthorizedException from './UnauthorizedException';
 import ForbiddenException from './ForbiddenException';
+import ConflictException from './ConflictException';
 
 export {
   NotFoundException,
   BadRequestException,
   UnauthorizedException,
   ForbiddenException,
+  ConflictException,
 };
