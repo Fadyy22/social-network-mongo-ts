9af96bb598563bc0625b648971d67ce6c0521767
Fadyy22 <fadyalaa441@gmail.com>
2024-10-22 14:52:10 +0300

    refactor validator

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -4,9 +4,10 @@ import { validationResult } from 'express-validator';
 export const globalValidatorMiddleware: RequestHandler = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
+    res
       .status(400)
       .json({ errors: errors.array(), message: 'Validation Failed' });
+    return;
   }
   next();
 };
