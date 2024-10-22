f46904856b555071aff3c3007b741513e7b271c3
Fadyy22 <fadyalaa441@gmail.com>
2024-10-22 16:26:03 +0300

    move validations to service

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,8 +1,15 @@
 import asyncHandler from 'express-async-handler';
 
+import { NotFoundException } from '../exceptions';
 import prisma from '../prisma';
 
 export const likePost = asyncHandler(async (req, res) => {
+  const post = await prisma.post.findUnique({
+    where: { id: req.params.id },
+    select: { id: true },
+  });
+  if (!post) throw new NotFoundException('Post not found');
+
   await prisma.like.create({
     data: {
       userId: req.user!.id,
@@ -21,10 +28,16 @@ export const likePost = asyncHandler(async (req, res) => {
     },
   });
 
-  res.status(201).json({ message: 'Post liked' });
+  res.status(201).json({ status: 'success' });
 });
 
 export const unlikePost = asyncHandler(async (req, res) => {
+  const post = await prisma.post.findUnique({
+    where: { id: req.params.id },
+    select: { id: true },
+  });
+  if (!post) throw new NotFoundException('Post not found');
+
   await prisma.like.delete({
     where: {
       postId_userId: {
--- src/likes/likes.validator.ts
+++ src/likes/likes.validator.ts
@@ -1,55 +1,13 @@
 import { check } from 'express-validator';
-import { PrismaClient } from '@prisma/client';
 
-import {
-  customValidatorMiddleware,
-  globalValidatorMiddleware,
-} from '../middlewares/validator.middleware';
-
-const prisma = new PrismaClient();
+import { globalValidatorMiddleware } from '../middlewares/validator.middleware';
 
 export const likePostValidator = [
-  check('id')
-    .isString()
-    .withMessage('Id must be a string')
-    .bail()
-    .custom(async (postId: string, { req }) => {
-      const post = await prisma.post.findUnique({
-        where: {
-          id: postId,
-        },
-      });
-
-      if (!post) {
-        return (req.customError = {
-          statusCode: 404,
-          message: 'Post not found',
-        });
-      }
-    }),
-  customValidatorMiddleware,
+  check('id').isString().withMessage('Id must be a string'),
   globalValidatorMiddleware,
 ];
 
 export const unlikePostValidator = [
-  check('id')
-    .isString()
-    .withMessage('Id must be a string')
-    .bail()
-    .custom(async (postId: string, { req }) => {
-      const post = await prisma.post.findUnique({
-        where: {
-          id: postId,
-        },
-      });
-
-      if (!post) {
-        return (req.customError = {
-          statusCode: 404,
-          message: 'Post not found',
-        });
-      }
-    }),
-  customValidatorMiddleware,
+  check('id').isString().withMessage('Id must be a string'),
   globalValidatorMiddleware,
 ];
