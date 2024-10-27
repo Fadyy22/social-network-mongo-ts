715a1fb67e890db9472fe0ce0fac65832b7c3bcf
Fadyy22 <fadyalaa441@gmail.com>
2024-10-27 21:48:10 +0300

    add user virtual to comment schema

--- src/comments/comment.model.ts
+++ src/comments/comment.model.ts
@@ -19,8 +19,20 @@ const commentSchema = new Schema(
   },
   {
     timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
+
+// We use this in getPost, where we populate the user
+// so that the user data are in "user" field insted of "userId"
+// This could be easily made by changing the filed name in commentSchema
+// from userId to user
+commentSchema.virtual('user', {
+  ref: 'User',
+  localField: 'userId',
+  foreignField: '_id',
+});
 
 const Comment = model('Comment', commentSchema);
 
