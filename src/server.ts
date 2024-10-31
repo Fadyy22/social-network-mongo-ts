520ef242a6803f9f8e250e12c9502fa8195b30b5
Fadyy22 <fadyalaa441@gmail.com>
2024-10-31 19:23:41 +0300

    compress responses

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import compression from 'compression';
 import dotenv from 'dotenv';
 import express from 'express';
 
@@ -13,6 +14,7 @@ const app = express();
 connectDB();
 
 app.use(cors());
+app.use(compression());
 app.use(express.json());
 
 mountRoutes(app);
