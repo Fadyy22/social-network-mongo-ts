a9a7cf2045d1fd4d5575618d6b1604742c7d9d95
Fadyy22 <fadyalaa441@gmail.com>
2024-11-05 15:52:20 +0200

    check for like existence in like and unlike

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,12 +1,18 @@
 import asyncHandler from 'express-async-handler';
 
-import { NotFoundException } from '../exceptions';
+import { ConflictException, NotFoundException } from '../exceptions';
 import Post from '../posts/post.model';
 import Like from './like.model';
 
 export const likePost = asyncHandler(async (req, res) => {
   const post = await Post.findById(req.params.id).select('_id likesCount');
   if (!post) throw new NotFoundException('Post not found');
+
+  const like = await Like.findOne({
+    userId: req.user!._id,
+    postId: req.params.id,
+  });
+  if (like) throw new ConflictException('Already liked');
 
   await Like.create({
     userId: req.user!._id,
@@ -23,10 +29,11 @@ export const unlikePost = asyncHandler(async (req, res) => {
   const post = await Post.findById(req.params.id).select('_id likesCount');
   if (!post) throw new NotFoundException('Post not found');
 
-  await Like.deleteOne({
+  const like = await Like.deleteOne({
     userId: req.user!._id,
     postId: req.params.id,
   });
+  if (!like.deletedCount) throw new NotFoundException('Not liked yet');
 
   post.likesCount--;
   await post.save();
