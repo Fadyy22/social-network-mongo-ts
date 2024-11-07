45067dd5f48038e8b9e90693a2bdbe4436b34270
Fadyy22 <fadyalaa441@gmail.com>
2024-11-07 18:32:49 +0200

    fix concurrency in likes endpoints

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -5,9 +5,6 @@ import Post from '../posts/post.model';
 import Like from './like.model';
 
 export const likePost = asyncHandler(async (req, res) => {
-  const post = await Post.findById(req.params.id).select('_id likesCount');
-  if (!post) throw new NotFoundException('Post not found');
-
   const like = await Like.findOne({
     userId: req.user!._id,
     postId: req.params.id,
@@ -19,24 +16,37 @@ export const likePost = asyncHandler(async (req, res) => {
     postId: req.params.id,
   });
 
-  post.likesCount++;
-  await post.save();
+  const post = await Post.findByIdAndUpdate(
+    req.params.id,
+    {
+      $inc: { likesCount: 1 },
+    },
+    { new: true }
+  );
+  if (!post) throw new NotFoundException('Post not found');
+  console.log(post);
+  console.log('inc');
 
   res.status(201).json({ status: 'success' });
 });
 
 export const unlikePost = asyncHandler(async (req, res) => {
-  const post = await Post.findById(req.params.id).select('_id likesCount');
-  if (!post) throw new NotFoundException('Post not found');
-
   const like = await Like.deleteOne({
     userId: req.user!._id,
     postId: req.params.id,
   });
   if (!like.deletedCount) throw new NotFoundException('Not liked yet');
 
-  post.likesCount--;
-  await post.save();
+  const post = await Post.findByIdAndUpdate(
+    req.params.id,
+    {
+      $inc: { likesCount: -1 },
+    },
+    { new: true }
+  );
+  if (!post) throw new NotFoundException('Post not found');
+  console.log(post);
+  console.log('dec');
 
   res.status(204).end();
 });
